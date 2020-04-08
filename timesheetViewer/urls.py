c5f1c2ebb4674a13e8c86baf6e90f09411d49e23
Andrew Holsaeter <hols@norceresearch.no>
2020-04-08 21:22:49 +0200

    Base html and projects route to display projects

---
+++ timesheetViewer/urls.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.urls import path
+
+# Create your views here.
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    # path("<int:pk>/", views.project_list, name="project_list"),
+    path('projects/', views.project_list, name='projects'),
+]
--- timesheetViewer/views.py
+++ timesheetViewer/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
+from timesheetViewer.models import Projects, SubProjects, TestDates
 
 # Create your views here.
+def index(request):
+    return render(request, "base.html")
+
+def project_list(request):
+    projects_obj = Projects.objects.all()
+    
+    context = {
+        "projects": projects_obj
+    }
+    return render(request, "projects.html",  context)
