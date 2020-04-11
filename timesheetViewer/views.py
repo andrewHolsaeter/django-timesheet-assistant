b79f025c1311ee8bf32d67123736e0a05edd5fd4
Andrew Holsaeter <hols@norceresearch.no>
2020-04-11 22:30:01 +0200

    Removed unused clock view

--- timesheetViewer/views.py
+++ timesheetViewer/views.py
@@ -63,38 +63,6 @@ class MultpleFormsTestView(MultiFormsView):
 def index(request):
     return render(request, "base.html")
 
-def clock(request):
-    # Change to selected
-    projects = Projects.objects.all()
-    sub_projects = SubProjects.objects.all()
-
-    # if this is a POST request we need to process the form data
-    if request.method == 'POST':
-        # create a form instance and populate it with data from the request:
-        form = TimesheetForm(request.POST)
-        # check whether it's valid:
-        if form.is_valid():
-            # process the data in form.cleaned_data as required
-            # ...
-            # book_instance.due_back = form.cleaned_data
-            # book_instance.save()
-            data = form.cleaned_data
-            # redirect to a new URL:
-            
-    # if a GET (or any other method) we'll create a blank form
-    else:
-        form = TimesheetForm(initial={'date': datetime.today(),'start_at':datetime.now()})
-    
-    dates = TestDates.objects.all()
-    context = {
-        "projects":projects,
-        "subprojects":sub_projects,
-        "form":form,
-        "dates": dates
-    }
-    
-    return render(request, "clock.html", context)
-
 def project_list(request):
     proj_sub_proj_obj = Projects.objects.raw("""
         SELECT p.id, p.name, array_agg(sp.description) as descriptions, array_agg(sp.id) as subprojects 
